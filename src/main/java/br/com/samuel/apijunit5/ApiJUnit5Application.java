2d995bd0b51dc47bf537727cf4035a0460bb96e3
samuel-melo1 <samuelprogramacao3@gmail.com>
2024-06-02 22:47:13 -0300

    initial commit

---
+++ src/main/java/br/com/samuel/apijunit5/ApiJUnit5Application.java
@@ -0,0 +1,13 @@
+package br.com.samuel.apijunit5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiJUnit5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiJUnit5Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/samuel/apijunit5/ApiJUnit5ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.samuel.apijunit5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiJUnit5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
