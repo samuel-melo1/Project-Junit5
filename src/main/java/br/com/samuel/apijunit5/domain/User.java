341c700e1d021c9049f0b6f584d794f36b44304c
samuel-melo1 <samuelprogramacao3@gmail.com>
2024-06-02 22:53:22 -0300

    implemented entity class user

---
+++ src/main/java/br/com/samuel/apijunit5/domain/User.java
@@ -0,0 +1,25 @@
+package br.com.samuel.apijunit5.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    @Column(unique = true)
+    private String email;
+    private String password;
+
+}
