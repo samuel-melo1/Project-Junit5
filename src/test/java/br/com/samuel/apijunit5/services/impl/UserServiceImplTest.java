c2a637d3717c4f1e5741828222f143e8df7e6695
samuel-melo1 <samuelprogramacao3@gmail.com>
2024-06-06 06:47:08 -0300

    testing to class user method findbyid

---
+++ src/test/java/br/com/samuel/apijunit5/services/impl/UserServiceImplTest.java
@@ -0,0 +1,73 @@
+package br.com.samuel.apijunit5.services.impl;
+
+import br.com.samuel.apijunit5.domain.User;
+import br.com.samuel.apijunit5.domain.dto.UserDTO;
+import br.com.samuel.apijunit5.repositories.UserRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+class UserServiceImplTest {
+
+    @InjectMocks
+    private UserServiceImpl service;
+    @Mock
+    private UserRepository repository;
+    @Mock
+    private ModelMapper mapper;
+    private User user;
+    private UserDTO userDTO;
+    private Optional<User> optionalUser;
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        startUser();
+    }
+
+    @Test
+    void whenFindByIdThenReturnAnUserInstance() {
+        when(repository.findById(Mockito.anyInt())).thenReturn(optionalUser);
+        User response = service.findById(1);
+
+        assertNotNull(response);
+        assertEquals(User.class, response.getClass());
+        assertEquals(1, response.getId());
+        assertEquals("samuel",response.getName());
+        assertEquals("samuel@gmail.com", response.getEmail());
+
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    private void startUser(){
+        user = new User(1, "samuel", "samuel@gmail.com", "1234");
+        userDTO = new UserDTO(1, "samuel", "samuel@gmail.com", "1234");
+        optionalUser = Optional.of(new User(1, "samuel", "samuel@gmail.com", "1234"));
+    }
+}
